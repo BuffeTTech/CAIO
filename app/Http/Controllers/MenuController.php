b5d3525fb7dff21d4e3f39fd43bec205e91f1815
Guilherme Andrade <guiandradeemail@gmail.com>
2025-02-18 14:15:07 -0300

    feat: sla

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreMenuRequest;
 use App\Http\Requests\UpdateMenuRequest;
+use App\Models\Menu\Item;
 use App\Models\Menu\Menu;
+use App\Models\Menu\MenuHasItem;
+use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
     public function __construct(
         protected Menu $menu,
+        protected Item $items,
     )
     {
     }
@@ -70,5 +74,64 @@ class MenuController extends Controller
     public function destroy(Menu $menu)
     {
         //
+    }
+
+    public function store_item_to_menu(Request $request) {
+        $menuItems = MenuHasItem::where('menu_id', $request->menu_id)->get();
+
+        // foreach ($menuItems as $item) {
+        //     $usedItem = [];
+        //     $numItem = random_int(0, 7);
+
+        //     for ($i = 0; $i < $numItem; $i++) {
+        //         do {
+        //             $randomIngredientId = $allIngredientIds[array_rand($allIngredientIds)];
+        //         } while (in_array($randomIngredientId, $usedItem));
+
+        //         $usedItem[] = $randomIngredientId;
+
+        //         // Adiciona relação para inserção em lote
+        //         $batchInsertRelations[] = [
+        //             "item_id" => $ingredientId,
+        //             "ingredient_id" => $randomIngredientId,
+        //             "observation" => "",
+        //             "quantity" => random_int(1, 3),
+        //         ];
+        //     }
+        // }
+
+        $item = MenuHasItem::create([
+            "menu_id"=>$request->menu_id,
+            "item_id"=>$request->item_id
+        ]);
+
+        // $client = Client::inRandomOrder()->first();
+        // $menu = Menu::inRandomOrder()->first();
+        // $event = Event::create([
+        //     "client_id"=>$client->id,
+        //     "menu_id"=>$menu->id,
+        //     "date"=>fake()->dateTimeBetween('now', '+4 months'),
+        //     "address_id" => random_int(0, 1) == 0 ? $client->address_id : Address::factory()->create()->id
+        // ]);
+
+        // $ingredientService = new CreateMenuEventService();
+        // $ingredientService->handle($event, $menu);
+        return redirect()->back();
+    }
+
+    public function add_item_to_menu(Request $request) {
+        $menu = $this->menu->find($request->menu_id);
+        if(!$menu) {
+            dd("Menu nao encontrado");
+        }
+
+        $items = null;
+        if($request->query('query')) {
+            $items = $this->items::when($request->has('query'), function ($whenQuery) use ($request) {
+                $whenQuery->where('name', 'like', '%' . $request->query('query') . '%');
+            })->orderByDesc('created_at')->paginate(10)->withQueryString();
+        }
+
+        return view('menu.add_item', compact('menu', 'items'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
 });
 
 Route::get('/menu',  [MenuController::class, 'index']);
+Route::post('/menu/{menu_id}',  [MenuController::class, 'store_item_to_menu'])->name('menu.store_item_to_menu');
+Route::get('/menu/{menu_id}',  [MenuController::class, 'add_item_to_menu'])->name('menu.add_item_to_menu');
+
+
 Route::get('/event/{event_id}/checklist',  [EventController::class, 'checklist'])->name('event.checklist');
 Route::delete('/menu/items/{item_id}/ingredients/{id}',  [IngredientController::class, 'destroy'])->name('ingredient.destroy');
 Route::delete('/menu/items/{item_id}/matherial/{id}',  [MatherialController::class, 'destroy'])->name('matherial.destroy');
@@ -25,7 +29,6 @@ Route::patch('/event/{event_id}/checklist/item/{item_id}/ingredient/{ingredient_
 Route::patch('/event/{event_id}/checklist/item/{item_id}/matherial/{matherial_id}', [EventController::class, 'check_matherial'])->name('event.checklist.check_matherial');
 Route::patch('/event/{event_id}/checklist/item/{item_id}', [EventController::class, 'check_item'])->name('event.checklist.check_item');
 Route::delete('/event/{event_id}/checklist/item/{item_id}', [EventController::class, 'delete_item'])->name('event.checklist.delete_item');
-
 
 Route::get('/teste', function() {
     $ingredientService = new CreateMenuEventService();
